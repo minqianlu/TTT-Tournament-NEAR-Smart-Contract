25ae938a072922f38b7fac9c06fc24d5e68462c8
Min Qian Lu <min@Mins-MacBook-Pro.local>
2022-07-15 17:48:26 -0400

    Laid out structure for tournament struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,47 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+//use std::convert::TryInto;
+//use std::ops::Index;
+
+use near_sdk::collections::{
+    LookupMap,
+    //Vector,
+};
+
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    //log,
+    //serde::{Deserialize, Serialize},
+    AccountId, PanicOnDefault
+    //, Promise,
+};
+//use near_sdk::env::{self, signer_account_id};
+use near_sdk::{near_bindgen};
+//use near_sdk::{near_bindgen, serde};
+use String;
+
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Tournament {
+    // SETUP CONTRACT STATE
+    player_stats: LookupMap<AccountId, u8>,
+    comp: bool,
+    round: u8,
+
+}
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Stats{
+    wins: u128,
+    losses: u128,
+    championships: u128,
+
+}
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     // SETUP CONTRACT STATE
+    tournament_keys: LookupMap<AccountId, String>,
+    tournaments: LookupMap<String, Tournament>,
+    user_stats: LookupMap<AccountId, Stats>
+
 }
 
 #[near_bindgen]
